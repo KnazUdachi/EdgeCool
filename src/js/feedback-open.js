4a234435ffd68d225e2b784506fce3643c2ba10c
Ваше Имя <_email@example.com>
2025-02-13 16:04:01 +0500

    Кляти свипер

---
+++ src/js/feedback-open.js
@@ -0,0 +1,51 @@
+// ОТКРЫВАЮЩЕЕСЯ ОКНО ОБРАТНОЙ СВЯЗИ
+const feedbackButton = document.querySelector('.side__contact-section-btn.side__contact-section-btn--chat')
+const feedback = document.querySelector('.feedback')
+const feedbackClose = document.querySelector('.feedback__btn')
+const feedbackLining = document.querySelector('.feedback__lining')
+const feedbackHeaderTablet = document.querySelector('.header__btn-chat')
+// import { body } from "./index.js";
+// import { menu } from "./index.js";
+const menu = document.querySelector('.side');
+const body = document.body;
+
+feedbackButton.addEventListener('click', function () {
+  feedback.classList.add('feedback-open')
+  body.classList.add('no-scroll');
+  setTimeout(() => {
+    feedback.classList.add('feedback-anim')
+  }, 10)
+})
+
+feedbackHeaderTablet.addEventListener('click', function () {
+  feedback.classList.add('feedback-open')
+  setTimeout(() => {
+    feedback.classList.add('feedback-anim')
+  }, 10)
+  body.classList.add('no-scroll');
+})
+
+
+feedbackClose.addEventListener('click', function () {
+  feedback.classList.remove('feedback-anim')
+  setTimeout(() => {
+    feedback.classList.remove('feedback-open')
+  }, 100)
+  if (!menu.classList.contains('menu-open')){
+    body.classList.remove('no-scroll');
+  }
+})
+
+
+feedback.addEventListener('click', function (event) {
+ 
+  if (!feedbackLining.contains(event.target)) {
+    feedback.classList.remove('feedback-anim')
+    setTimeout(() => {
+      feedback.classList.remove('feedback-open')
+    }, 100)
+    if (!menu.classList.contains('menu-open')){
+      body.classList.remove('no-scroll');
+    }
+  }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,105 +1,24 @@
-import '../scss/style.scss'
-
-console.log('Закончили работу')
-
-let swiperInstance
-
-function initSwiper() {
-  if (window.innerWidth < 768 && !swiperInstance) {
-    swiperInstance = new Swiper('.swiper', {
-      direction: 'horizontal',
-      loop: true,
-
-      slidesPerView: 'auto',
-
-      pagination: {
-        el: '.swiper-pagination'
-      },
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-
-      scrollbar: {
-        el: '.swiper-scrollbar'
-      }
-    })
-  } else if (window.innerWidth >= 767 && swiperInstance) {
-    swiperInstance.destroy(true, true)
-    swiperInstance = null
-  }
-}
-document.addEventListener('DOMContentLoaded', initSwiper)
-window.addEventListener('resize', initSwiper)
-
-const wrapperLogoDesctop = document.querySelectorAll('.desctop__table')
-const wrapperLogoHidden = document.querySelectorAll('.hidden')
-const wrapperLogo = document.querySelectorAll('.hidden, .desctop__table')
-
-const button = document.querySelector('.button-open')
-
-button.addEventListener('click', function () {
-  const isHidden = button.classList.contains('button-close')
-
-  if (isHidden) {
-    wrapperLogoHidden.forEach((element) => {
-      element.classList.add('hidden')
-    })
-    wrapperLogoDesctop.forEach((element) => {
-      element.classList.add('desctop__table')
-    })
-
-    button.textContent = 'Показать все'
-  } else {
-    wrapperLogo.forEach((element) => {
-      element.classList.remove('desctop__table', 'hidden')
-    })
-
-    button.textContent = 'Скрыть'
-  }
-
-  button.classList.toggle('button-close')
-})
+import '../scss/style.scss';
+import './swiper.js';
+import './button.js';
+import './side-open.js';
+import './feedback-open.js';
+import './call-open.js';
 
 
-  const toggleButton = document.querySelector(".repair__button");
-  const repairItems = document.querySelectorAll(".repair__wrapper:nth-child(n+4)"); 
-
-  toggleButton.addEventListener("click", function () {
-    const isExpanded = toggleButton.textContent === "Показать все";
-
-    repairItems.forEach((item) => item.classList.toggle("visible", isExpanded));
-    toggleButton.textContent = isExpanded ? "Скрыть" : "Показать все";
-    toggleButton.classList.toggle("expanded", isExpanded);
-  });
 
 
-// ОТКРЫВАЮЩЕЕСЯ МЕНЮ
-const hamburgerButton = document.querySelector('.header__menu-btn')
-const menu = document.querySelector('.side')
-const sideMenu = document.querySelector('.side__menu-btn')
-const body = document.body
-const sideLining = document.querySelector('.side__lining'); 
 
-hamburgerButton.addEventListener('click', function () {
-  menu.classList.add('menu-open');
-  body.classList.add('no-scroll');
-});
-
-sideMenu.addEventListener('click', function () {
-  menu.classList.remove('menu-open');
-  body.classList.remove('no-scroll');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const body = document.body;
+//   const menu = document.querySelector('.side');
+  
+//   console.log(menu);
 
 
-menu.addEventListener('click', function (event) {
- 
-  if (!sideLining.contains(event.target)) {
-    menu.classList.remove('menu-open');
-    body.classList.remove('no-scroll');
-  }
-});
+// });
+
+// export { body, menu };
 
 
 
@@ -107,105 +26,4 @@ menu.addEventListener('click', function (event) {
 
 
 
-// ОТКРЫВАЮЩЕЕСЯ ОКНО ОБРАТНОЙ СВЯЗИ
-const feedbackButton = document.querySelector('.side__contact-section-btn.side__contact-section-btn--chat')
-const feedback = document.querySelector('.feedback')
-const feedbackClose = document.querySelector('.feedback__btn')
-const feedbackLining = document.querySelector('.feedback__lining')
-const feedbackHeaderTablet = document.querySelector('.header__btn-chat')
 
-
-
-feedbackButton.addEventListener('click', function () {
-  feedback.classList.add('feedback-open')
-  body.classList.add('no-scroll');
-  setTimeout(() => {
-    feedback.classList.add('feedback-anim')
-  }, 10)
-})
-
-feedbackHeaderTablet.addEventListener('click', function () {
-  feedback.classList.add('feedback-open')
-  setTimeout(() => {
-    feedback.classList.add('feedback-anim')
-  }, 10)
-  body.classList.add('no-scroll');
-})
-
-
-feedbackClose.addEventListener('click', function () {
-  feedback.classList.remove('feedback-anim')
-  setTimeout(() => {
-    feedback.classList.remove('feedback-open')
-  }, 100)
-  if (!menu.classList.contains('menu-open')){
-    body.classList.remove('no-scroll');
-  }
-})
-
-
-feedback.addEventListener('click', function (event) {
- 
-  if (!feedbackLining.contains(event.target)) {
-    feedback.classList.remove('feedback-anim')
-    setTimeout(() => {
-      feedback.classList.remove('feedback-open')
-    }, 100)
-    if (!menu.classList.contains('menu-open')){
-      body.classList.remove('no-scroll');
-    }
-  }
-});
-
-
-
-
-
-const callButton = document.querySelector(  '.side__contact-section-btn.side__contact-section-btn--call')
-const call = document.querySelector('.call')
-const callClose = document.querySelector('.call__btn')
-const callLining = document.querySelector('.call__lining')
-const callHeaderTablet = document.querySelector('.header__btn-call')
-
-callButton.addEventListener('click', function () {
-  call.classList.add('call-open')
-  body.classList.add('no-scroll');
-  setTimeout(() => {
-    call.classList.add('call-anim')
-  }, 10)
-})
-
-callHeaderTablet.addEventListener('click', function () {
-  call.classList.add('call-open')
-  setTimeout(() => {
-    call.classList.add('call-anim')
-  }, 10)
-  body.classList.add('no-scroll');
-})
-
-
-
-
-callClose.addEventListener('click', function () {
-  call.classList.remove('call-anim')
-  setTimeout(() => {
-    call.classList.remove('call-open')
-  }, 100)
-
-  if (!menu.classList.contains('menu-open')){
-    body.classList.remove('no-scroll');
-  }
-
-})
-
-call.addEventListener('click', function(event){
-  if(!callLining.contains(event.target)){
-    call.classList.remove('call-anim')
-    setTimeout(() => {
-      call.classList.remove('call-open')
-    }, 100)
-  }
-  if (!menu.classList.contains('menu-open')){
-    body.classList.remove('no-scroll');
-  }
-})
